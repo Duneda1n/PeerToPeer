2a9ac2acd9473411fd00d4aa42e2577cb7963e54
Κυριάκος <Sidiropoulos.Kyriakos@outlook.com.gr>
2020-04-19 20:57:16 +0300

    Peer To Peer Library

--- app/src/main/java/com/example/peertopeer/MainActivity.java
+++ app/src/main/java/com/example/peertopeer/MainActivity.java
@@ -51,9 +51,18 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     //Sending the user to MyLibrary class
-    public  void go(View g)
+    public  void goA(View g)
+    {
+        Intent i=new Intent(this,RentABook.class);
+        startActivity(i);
+    }
+    public  void goB(View g)
     {
         Intent i=new Intent(this,MyLibrary.class);
         startActivity(i);
     }
+    public  void goC(View g)
+    {
+      Intent i=new Intent(this,BookShop.class);
+     startActivity(i);}
 }
