6846f0e0aa6d3a67d57c9dfb4cbed048c0b5d4f6
Κυριάκος <Sidiropoulos.Kyriakos@outlook.com.gr>
2020-05-10 16:42:58 +0300

    Peer To Peer Library

--- app/src/main/java/com/example/peertopeer/BookShop.java
+++ app/src/main/java/com/example/peertopeer/BookShop.java
@@ -5,12 +5,19 @@ import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class BookShop extends AppCompatActivity {
+    ArrayAdapter<String> arrayAdapter;
     //adding toolbar to this activity
     private Toolbar toolbar;
     @Override
@@ -19,6 +26,21 @@ public class BookShop extends AppCompatActivity {
         setContentView(R.layout.activity_book_shop);
         toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+
+        ListView listView = findViewById(R.id.my_list);
+        ArrayList<String> mylist = new ArrayList<>();
+        mylist.add("Oliver Twist");
+        mylist.add("Silmarillion");
+        mylist.add("War and Peace");
+        mylist.add("Hamlet");
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateThumbnail(Bitmap outBitmap, Canvas canvas) {
+        return super.onCreateThumbnail(outBitmap, canvas);
     }
 
     //Adding menu on toolbar with inflator
@@ -26,10 +48,24 @@ public class BookShop extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_main,menu);
-        MenuItem searchItem = menu.findItem(R.id.search_bar);
-        return true;
+        MenuItem menuItem = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
-        };
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter();
+                return false;
+
+            }
+        });
+        return true;
+    }
+
     //  Action bar item clicks are being handled here.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
